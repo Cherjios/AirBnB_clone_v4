f69a5eb50c981647cbfbdb9a36d2a52d867f40ea
alaksatti <ala.k.satti@gmail.com>
2020-02-17 22:33:20 +0000

    semistandard compliant

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-  let amenityid = {};
+  const amenityid = {};
   $('input[type="checkbox"]').click(function () {
     if ($(this).is(':checked')) {
       amenityid[$(this).attr('data-id')] = $(this).attr('data-name');
     } else if ($(this).is(':not(:checked)')) {
       delete amenityid[$(this).attr('data-id')];
     }
-    let amenitylist = [];
-    for (let key in amenitylist) {
+    const amenitylist = [];
+    for (const key in amenitylist) {
       amenitylist.push(amenityid[key]);
     }
     $('.amenities h4').text(amenitylist.join(', '));
--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-  let amenityid = {};
+  const amenityid = {};
   $('input[type="checkbox"]').click(function () {
     if ($(this).is(':checked')) {
       amenityid[$(this).attr('data-id')] = $(this).attr('data-name');
     } else if ($(this).is(':not(:checked)')) {
       delete amenityid[$(this).attr('data-id')];
     }
-    let amenitylist = [];
-    for (let key in amenitylist) {
+    const amenitylist = [];
+    for (const key in amenitylist) {
       amenitylist.push(amenityid[key]);
     }
     $('.amenities h4').text(amenitylist.join(', '));
   });
   $.get('http://0.0.0.0:5001/api/v1/status/', function (response) {
-    if (data.response === 'OK') {
+    if (response.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').toggleClass('available');
